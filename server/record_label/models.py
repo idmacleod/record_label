44a5b9e2aae58835172d114debdad38390da8a4b
idmacleod <idmacleod@gmail.com>
2020-08-07 14:59:13 +0100

    Added initial models and migration, ran migrate

--- server/record_label/models.py
+++ server/record_label/models.py
@@ -1,3 +1,25 @@
 from django.db import models
 
-# Create your models here.
+class Artist(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+
+class Release(models.Model):
+    short_name = models.CharField(max_length=100)
+    long_name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    artist = models.ForeignKey(Artist, on_delete=models.CASCADE)
+
+class BlogPost(models.Model):
+    pub_date = models.DateTimeField()
+    category = models.CharField(max_length=100)
+    text = models.CharField(max_length=1000)
+    media = models.CharField(max_length=200)
+    links = models.CharField(max_length=200)
+    release = models.ForeignKey(Release, on_delete=models.CASCADE)
+
+class Edition(models.Model):
+    name = models.CharField(max_length=200)
+    description = models.CharField(max_length=200)
+    release = models.ForeignKey(Release, on_delete=models.CASCADE)
+
