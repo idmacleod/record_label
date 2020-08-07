7031ac949572480cca4b9bf6ec99877e649da179
idmacleod <idmacleod@gmail.com>
2020-08-07 16:00:47 +0100

    Registering models in admin.py

--- server/record_label/admin.py
+++ server/record_label/admin.py
@@ -1,3 +1,7 @@
 from django.contrib import admin
+from .models import *
 
-# Register your models here.
+admin.site.register(Artist)
+admin.site.register(Release)
+admin.site.register(BlogPost)
+admin.site.register(Edition)
