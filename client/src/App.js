be99735432f5b3a2ab4b61a111fd4140c637643a
idmacleod <idmacleod@gmail.com>
2020-08-10 16:25:11 +0100

    Installing django-cors-headers and whitelisting localhost:3000, fetching artists data into state

--- client/src/App.js
+++ client/src/App.js
@@ -1,14 +1,38 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <h1>Record Label</h1>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      artists: [],
+      releases: [],
+      editions: [],
+      posts: []
+    }
+
+    this.fetchData = this.fetchData.bind(this);
+  }
+
+  fetchData() {
+    fetch('http://localhost:8000/artists/')
+      .then(res => res.json())
+      .then(data => this.setState({artists: data}));
+  }
+
+  componentDidMount() {
+    this.fetchData();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <h1>Record Label</h1>
+        </header>
+      </div>
+    );
+  }
 }
 
 export default App;
