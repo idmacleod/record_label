4364d4c7ec1dcf70cbee3a69769ede73a6afd030
idmacleod <idmacleod@gmail.com>
2020-08-12 11:56:43 +0100

    Starting on left-hand side, adding images to .gitignore

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -1,10 +1,15 @@
 import React from 'react';
-import '../styles/Intro.css'
+import '../styles/Intro.css';
+import image1 from '../images/FIREC014LP_BAG.jpg';
 
 const Intro = () => {
     return (
         <div className="intro">
-            <h1>tending the void</h1>
+            <header>
+                <h1>tending the void</h1>
+                <img src={image1} alt=""/>
+                <img src={image1} alt=""/>
+            </header>
             <p>Edinburgh based label specialising in oddball house, techno and electronics. Collectable editions made by hand since 2004.</p>
         </div>
     );
