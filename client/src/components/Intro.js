dbeba7a50c61bb799a2cf01a8288e75220974ae1
idmacleod <idmacleod@gmail.com>
2020-08-12 13:55:36 +0100

    Added grid layout to Intro component

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -3,16 +3,17 @@ import '../styles/Intro.css';
 import image1 from '../images/FIREC014LP_BAG.jpg';
 
 const Intro = () => {
-    return (
-        <div className="intro">
-            <header>
-                <h1>tending the void</h1>
-                <img src={image1} alt=""/>
-                <img src={image1} alt=""/>
-            </header>
-            <p>Edinburgh based label specialising in oddball house, techno and electronics. Collectable editions made by hand since 2004.</p>
-        </div>
-    );
+  return (
+    <div className="intro">
+      <header id="title">
+        <h1>tending the void</h1>
+      </header>
+      <img src={image1} id="img1" alt="" />
+      <img src={image1} id="img2" alt="" />
+      <img src={image1} id="img3" alt="" />
+      <p id="description">Edinburgh based label specialising in oddball house, techno and electronics. Collectable editions made by hand since 2004.</p>
+    </div>
+  );
 }
 
 export default Intro;
