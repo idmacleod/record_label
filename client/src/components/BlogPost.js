ba399af97fabc11d680c4849afac7cc45fb86697
idmacleod <idmacleod@gmail.com>
2020-08-12 17:48:34 +0100

    Finished initial styling of BlogPost

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -2,11 +2,12 @@ import React from 'react';
 import moment from 'moment';
 
 import ReleaseDetails from './ReleaseDetails';
+import '../styles/BlogPost.css'
 
 const BlogPost = ({post}) => {
   return (
     <div className="blog-post">
-      <h3 className="title">{moment(post.pub_date).format('MMM Do YYYY')} - {post.title}</h3>
+      <h3 className="title"><span className="date">{moment(post.pub_date).format('MMM Do YYYY')}</span> {post.title}</h3>
       <p className="text">{post.text}</p>
       {post.release && <ReleaseDetails release={post.release} />}
     </div>
