2410f0ca8c7e1b87b72488ed211b634c1b2e4733
idmacleod <idmacleod@gmail.com>
2020-08-12 09:42:04 +0100

    Style changes

--- client/src/components/Intro.js
+++ client/src/components/Intro.js
@@ -4,7 +4,7 @@ import '../styles/Intro.css'
 const Intro = () => {
     return (
         <div className="intro">
-            <h1>Tending The Void</h1>
+            <h1>tending the void</h1>
             <p>Edinburgh based label specialising in oddball house, techno and electronics. Collectable editions made by hand since 2004.</p>
         </div>
     );
