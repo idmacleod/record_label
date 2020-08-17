6a41994f7ec1c999b40ab1c88d0ef7ed6e78fd66
idmacleod <idmacleod@gmail.com>
2020-08-17 11:11:29 +0100

    Updated BlogPost to display media

--- client/src/components/BlogPost.js
+++ client/src/components/BlogPost.js
@@ -9,6 +9,7 @@ const BlogPost = ({post}) => {
     <div className="blog-post">
       <h3 className="title"><span className="date">{moment(post.pub_date).format('MMM Do YYYY')}</span> {post.title}</h3>
       <p className="text">{post.text}</p>
+      {post.media && <img src={'http://localhost:8000' + post.media} alt={post.title} /> }
       {post.release && <ReleaseDetails release={post.release} />}
     </div>
   );
