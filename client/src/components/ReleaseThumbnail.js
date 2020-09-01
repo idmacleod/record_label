e46047bfbc6adaa1c9576d080485254c5521e153
idmacleod <idmacleod@gmail.com>
2020-09-01 14:19:10 +0100

    Adding hover animation to ReleaseThumbnail

--- client/src/components/ReleaseThumbnail.js
+++ client/src/components/ReleaseThumbnail.js
@@ -1,13 +1,14 @@
 import React from 'react';
+import { motion } from 'framer-motion';
 
 import '../styles/ReleaseThumbnail.css';
 
 const ReleaseThumbnail = ({index, release, onSelect}) => {
   return (
-    <li className="release-thumbnail" onClick={() => onSelect(index)}>
+    <motion.li className="release-thumbnail" onClick={() => onSelect(index)} whileHover={{ scale: 1.05 }}>
       <img className="thumbnail" src={release.cover_art} alt="cover art" />
       <p>{release.name}</p>
-    </li>
+    </motion.li>
   );
 }
 
