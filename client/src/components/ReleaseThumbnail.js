2aaf0b52b12064a8826a57993ed15d592a65475c
idmacleod <idmacleod@gmail.com>
2020-09-13 16:57:38 +0100

    Removing animation from ReleaseThumbnail

--- client/src/components/ReleaseThumbnail.js
+++ client/src/components/ReleaseThumbnail.js
@@ -1,11 +1,10 @@
 import React from 'react';
-import { motion } from 'framer-motion';
 
 import '../styles/ReleaseThumbnail.css';
 
 const ReleaseThumbnail = ({index, release, onSelect}) => {
   return (
-    <motion.li className="release-thumbnail" onClick={() => onSelect(index)} whileHover={{ scale: 1.05 }}>
+    <li className="release-thumbnail" onClick={() => onSelect(index)}>
       <div className="left">
         <img className="thumbnail" src={release.cover_art} alt="cover art" />
         <p>{release.name}</p>
@@ -13,7 +12,7 @@ const ReleaseThumbnail = ({index, release, onSelect}) => {
       <div className="right">
         <p>~ ~ ~ {release.artist.name}</p>
       </div>
-    </motion.li>
+    </li>
   );
 }
 
