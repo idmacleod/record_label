fae9bd5142cd17228b619efd76bd4fd58fdae5b7
idmacleod <idmacleod@gmail.com>
2020-09-13 15:27:13 +0100

    Setting soundcloud widgets to hide related

--- client/src/components/MixList.js
+++ client/src/components/MixList.js
@@ -12,9 +12,9 @@ const MixList = () => {
         initial={{ opacity: 0 }}
         animate={{ opacity: 1 }}
       >
-        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
-        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
-        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=false&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
+        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
+        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
+        <MixTrack track="https://w.soundcloud.com/player/?url=https%3A//api.soundcloud.com/tracks/760812571&color=%231a262c&auto_play=false&hide_related=true&show_comments=false&show_user=false&show_reposts=false&show_teaser=false&show_artwork=false" />
       </motion.ul>
     </AnimatePresence>
   );
